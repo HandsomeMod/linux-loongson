328975e8d02ff1de39fc4cd4946bd62cdf7f0e0f
handsomeyingyan <handsomeyingyan@github.com>
2021-05-01 21:20:51 +0800

    LS2K DTB

--- arch/mips/include/asm/mach-loongson64/builtin_dtbs.h
+++ arch/mips/include/asm/mach-loongson64/builtin_dtbs.h
@@ -8,6 +8,7 @@
 #ifndef __ASM_MACH_LOONGSON64_BUILTIN_DTBS_H_
 #define __ASM_MACH_LOONGSON64_BUILTIN_DTBS_H_
 
+extern u32 __dtb_loongson2k_begin[];
 extern u32 __dtb_loongson64c_4core_ls7a_begin[];
 extern u32 __dtb_loongson64c_4core_rs780e_begin[];
 extern u32 __dtb_loongson64c_8core_rs780e_begin[];
